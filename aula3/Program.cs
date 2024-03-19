69e92ea19bc8bbf67b2b8d61590f7e5124c5f7e6
Rafael <rafael.pilato.pi@gmail.com>
2024-03-18 21:46:59 -0300

    aula 3

---
+++ aula3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Instanciar uma Universidade
+using Microsoft.VisualBasic;
+
+Universidade objUniversidade = new Universidade();
+objUniversidade.nome = "Up";
+objUniversidade.localizacao = "Ecoville";
+objUniversidade.anofundacao = 1990;
+
+//Criar o curso ads e adicionar na universidade
+Curso objCurso = new Curso();
+objCurso.nome = "ADS";
+objUniversidade.AdicionarCurso(objCurso);
+
+//Criar o curso BSI e adicionar na universidade
+objCurso = new Curso();
+objCurso.nome="BSI";
+objUniversidade.AdicionarCurso(objCurso);
+
+Console.WriteLine(objUniversidade.nome);
+foreach(var curso in objUniversidade.cursos){
+    Console.WriteLine(curso.nome);
+}
